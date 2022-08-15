9973507cc0c825bb94c3f34b022f0a59cf8ef2b8
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2022-08-15 17:46:30 +0300

    task-1-smthDone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.append(...createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description, }) => {
+        const linkEl = document.createElement('a');
+        linkEl.classList.add('gallery__link');
+        linkEl.href = original;        
+
+        const imgEl = document.createElement('img');
+        imgEl.classList.add('gallery__image');
+        imgEl.src = preview;        
+        imgEl.alt = description;        
+
+        linkEl.append(imgEl);        
+
+        return linkEl;
+    });
+}
+
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.options.captionsData = 'alt';
+gallery.options.captionDelay = 250;
