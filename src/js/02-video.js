157a4d5a27ffc00e18d3d79fd45bfb19dee1dc17
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2022-08-15 22:55:11 +0300

    task-2-smthDone

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY));
+}
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, seconds); 
+        
+        }).catch(function(error) {
+            console.log(error);
+        });
+    
+}, 1000));
